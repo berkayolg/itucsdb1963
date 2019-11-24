f02c52561e6ee6282a0ab0b12a1e534ed19aee8b
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2019-11-24 17:11:04 +0300

    Started working on front-end.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -6,7 +7,91 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello, world!"
+    """
+    This is the first page users get. We will have a login form here.
+
+    :return:
+    """
+    return render_template("home.html")
+
+
+@app.route("/add_course")
+def add_course():
+    """
+    This is for professors to add a new course and relevant information about it e.g. date, TA's etc.
+    :return:
+    """
+    return render_template("add_course.html")
+
+
+@app.route("/grades")
+def grades():
+    """
+    This webpage is intended for all users.
+    Students -> Will be able to see their grades
+    Assistants -> Will be able to see their grades(for grad level courses) and enter grades for courses they are TA'ing
+    Professors -> Will be able to enter grades (choose a course, get a list of students)
+    :return:
+    """
+    return render_template("grades.html")
+
+
+@app.route("/courses")
+def courses():
+    """
+    This page is common for users.
+
+    If user is student, she will see the courses she is registered to.
+    If user is an assistant, she will see the courses she is registered to and she is TA'ing.
+    If user is a professor, she will see the courses she is giving.
+
+    :return:
+    """
+    return render_template("courses.html")
+
+
+@app.route("/settings")
+def user_settings():
+    """
+    Users will be able to update their profile pictures, phone number etc.
+
+    :return:
+    """
+    return render_template("settings.html")
+
+
+@app.route("/schedule")
+def schedule():
+    """
+    This is a common page. Users will be able to see their schedules according to courses
+    they are registered to/they are giving.
+
+    :return:
+    """
+    return render_template("schedule.html")
+
+
+@app.route("/exams")
+def exams():
+    """
+    In this page we will show the exam dates for students.
+    Same for assistants.
+
+    Professors will see exam dates of their courses and they will be able to update the exam date if it is not
+    colliding with another exam date.
+
+    :return:
+    """
+    return render_template("exams.html")
+
+
+@app.route("/su")
+def admin_page():
+    """
+    God mode.
+    :return:
+    """
+    return render_template("admin_page.html")
 
 
 if __name__ == "__main__":
