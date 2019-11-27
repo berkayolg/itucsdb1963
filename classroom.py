c2b5f436c7437b6d7c58ac110b46271acc16b469
berkayolg <berkayolg@hotmail.com>
2019-11-27 17:08:42 +0300

    Added room and classroom model classes with forms

---
+++ classroom.py
@@ -0,0 +1,4 @@
+class Classroom:
+    def __init__(self, id, building):
+        self.id = id
+        self.building = building
---
+++ room.py
@@ -0,0 +1,9 @@
+class Room:
+    def __init__(self, id, building, name, cap, classroom = False, room = False , lab = False):
+        self.id = id
+        self.building = building
+        self.name = name
+        self.cap = cap
+        self.classroom = classroom
+        self.room = room
+        self.lab = lab
