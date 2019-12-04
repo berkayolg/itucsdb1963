e5a4e8d86980f9cac6f423b225a64186440d4107
mehmetaltuner <altuner17@itu.edu.tr>
2019-12-04 21:13:23 +0300

    Internal Server Error bug fixes cont.

--- server.py
+++ server.py
@@ -192,9 +192,8 @@ def student_create():
 
 @app.route("/student_list", methods = ["GET", ])
 def students_list():
-    db = current_app.config["db"]
+    #db = current_app.config["db"]
     #students = db.get_students().values()
-    students = [1, 2, 3]
     return render_template("students_list.html", students = students)
 
 if __name__ == "__main__":
