3b303bc1297a9b69115c62f8d6136b2bec04ad31
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2019-12-04 21:46:00 +0300

    Added class blueprints

---
+++ models/lab.py
@@ -0,0 +1,7 @@
+class Lab:
+    def __init__(self, name, department, faculty, room, investigator):
+        self.name = name
+        self.department = department
+        self.faculty = faculty
+        self.room = room
+        self.investigator = investigator
