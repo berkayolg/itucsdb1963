989bfb58450e6e552c84fe7a4c70f950b4e54b5a
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2019-12-15 18:32:12 +0300

    Small fix

--- database.py
+++ database.py
@@ -1062,14 +1062,14 @@ class Database:
                     val = {
                         "ID": datum[0],
                         "Title": datum[1].strip('"'),
-                        "Platform": datum[2],
+                        "Platform": datum[2].strip('"'),
                         "Authors": []
                     }
                     retval.append(val)
 
                 for val in retval:
                     cursor = connection.cursor()
-                    statement = "SELECT (name) FROM papers p1 JOIN papers p2 ON p1.title = p2.title JOIN people p3 ON p3.p_id = p1.author WHERE p1.author <> p2.author AND p1.title = %s"
+                    statement = "SELECT name FROM papers p1 JOIN papers p2 ON p1.title = p2.title JOIN people p3 ON p3.p_id = p1.author WHERE p1.author <> p2.author AND p1.title = %s"
                     data = [val["Title"]]
                     cursor.execute(statement, data)
                     data = cursor.fetchall()
